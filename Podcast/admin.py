d8dff17572d0e2ae9e2a6f4be57260a844981399
Rahman-Nayemur64 <21201164@uap-bd.edu>
2024-04-15 02:56:58 +0600

    app created

--- Podcast/admin.py
+++ Podcast/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Episode, Playlist, Premium, Subscription
+from .models import Creator
 
 # Register your models here.
 
-admin.site.register([Episode, Playlist, Premium, Subscription])
+admin.site.register([Episode, Playlist, Premium, Subscription,Creator])
